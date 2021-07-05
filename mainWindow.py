8a87e47f7d4c7aea02f96b0d217ac9ff5ddcd6c5
hunter-a1 <78240696+hunter-a1@users.noreply.github.com>
2021-07-04 17:48:58 -0700

    add main program and window files

---
+++ mainWindow.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QDialog
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+    #end __init__
+    def setupUI(self):
+        pass
