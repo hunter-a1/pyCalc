a9522340114e5bf5231b425b8208210d772274a8
hunter-a1 <78240696+hunter-a1@users.noreply.github.com>
2021-07-06 16:47:27 -0700

    add signals, equals works

--- mainWindow.py
+++ mainWindow.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QLineEdit
 class MainWindow(QDialog):
     def __init__(self):
         super().__init__()
+        self.operatorsList = ['+','-','x','/']
         self.setupUI()
     #end __init__
     def setupUI(self):
@@ -19,25 +20,41 @@ class MainWindow(QDialog):
         self.sevenBtn = QPushButton('7')
         self.sevenBtn.clicked.connect(self.slotNumClicked)
         self.eightBtn = QPushButton('8')
+        self.eightBtn.clicked.connect(self.slotNumClicked)
         self.nineBtn = QPushButton('9')
+        self.nineBtn.clicked.connect(self.slotNumClicked)
 
         self.fourBtn = QPushButton('4')
+        self.fourBtn.clicked.connect(self.slotNumClicked)
         self.fiveBtn = QPushButton('5')
+        self.fiveBtn.clicked.connect(self.slotNumClicked)
         self.sixBtn = QPushButton('6')
+        self.sixBtn.clicked.connect(self.slotNumClicked)
 
         self.oneBtn = QPushButton('1')
+        self.oneBtn.clicked.connect(self.slotNumClicked)
         self.twoBtn = QPushButton('2')
+        self.twoBtn.clicked.connect(self.slotNumClicked)
         self.threeBtn = QPushButton('3')
+        self.threeBtn.clicked.connect(self.slotNumClicked)
 
         self.zeroBtn = QPushButton('0')
+        self.zeroBtn.clicked.connect(self.slotNumClicked)
         self.decBtn = QPushButton('.')
+        self.decBtn.clicked.connect(self.slotNumClicked)
         self.plusBtn = QPushButton('+')
+        self.plusBtn.clicked.connect(self.slotNumClicked)
         self.minusBtn = QPushButton('-')
+        self.minusBtn.clicked.connect(self.slotNumClicked)
         self.multBtn = QPushButton('x')
+        self.multBtn.clicked.connect(self.slotNumClicked)
         self.divBtn = QPushButton('/')
+        self.divBtn.clicked.connect(self.slotNumClicked)
         self.delBtn = QPushButton('del')
+        self.delBtn.clicked.connect(self.slotDelClicked)
 
         self.equalsBtn = QPushButton('=')
+        self.equalsBtn.clicked.connect(self.slotEqualsClicked)
 
         #
         #  Layout
@@ -77,6 +94,109 @@ class MainWindow(QDialog):
             self.outLE.setText(leTxt)
         #end if
     #end slotNumClicked
+    def slotDelClicked(self):
+        leTxt = self.outLE.text()
+        leTxt = leTxt[:-1]
+        self.outLE.setText(leTxt)
+    #end slotDelClicked
+    def slotEqualsClicked(self):
+        eqTxt = self.outLE.text()
+        operatorIndxList = []
+        iKtr = 0
+        for chr in eqTxt:
+            if chr in self.operatorsList:
+                operatorIndxList.append(iKtr)
+            #end if
+            iKtr += 1
+        #next
+        floatList = []
+        eqList = []
+        prevIndx = 0
+        for indx in operatorIndxList:
+            thisFloat = float(eqTxt[prevIndx:indx])
+            floatList.append(thisFloat)
+            eqList.append(thisFloat)
+            eqList.append(eqTxt[indx])
+            prevIndx = indx + 1
+        #next
+        lastFloat = float(eqTxt[indx+1:])
+        eqList.append(lastFloat)
+
+        x = eqList[0]
+        func = eqList[1]
+        y = eqList[2]
+        if func == '+':
+            out = self.add(x,y)
+        elif func == '-':
+            out = self.subtract(x,y)
+        elif func == 'x':
+            out = self.multiply(x,y)
+        elif func == '/':
+            out = self.divide(x,y)
+        else:
+            print('operation not recognized: ',func)
+        #end if
+
+        # thisFloatStr = ''
+        # calcList = []
+        # for chr in eqTxt:
+        #     if chr not in self.operatorsList:
+        #         thisFloatStr += chr
+        #     else:
+        #         thisFloat = float(thisFloatStr)
+        #         calcList.append(thisFloat)
+        #         calcList.append(chr)
+        #         thisFloatStr = ''
+        #     #end if
+        # #next
+
+
+        # x = None
+        # y = None
+        # func = None
+        # for c in calcList:
+        #     if isinstance(c, float):
+        #         if x is None:
+        #             x = c
+        #         elif y is None:
+        #             y = c
+        #             if func is None:
+        #                 print("improper syntax")
+        #             else:
+        #                 if func == '+':
+        #                     x = self.add(x,y)
+        #                 elif func == '-':
+        #                     x = self.subtract(x,y)
+        #                 elif func == 'x':
+        #                     x = self.multiply(x,y)
+        #                 elif func == '/':
+        #                     x = self.divide(x,y)
+        #                 else:
+        #                     print('operation not recognized: ',func)
+        #                 #end if
+        #                 y = None
+        #                 func = None
+        #             #end if
+        #         #end if
+        #     elif isinstance(c,str):
+        #         func = c
+        #     #end if
+        # #next c
+
+        self.outLE.setText(str(out))
+    #end slotEqualsClicked
+    def add(self,x,y):
+        return x + y
+    #end add
+    def subtract(self,x,y):
+        return x - y
+    #end subtract
+    def multiply(self,x,y):
+        return x * y
+    #end multiply
+    def divide(self,x,y):
+        return x / y
+    #end divide
 
 
 
